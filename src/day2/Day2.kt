ae5f211fee9277a7942a7983f4c7b6a1285d3b40
Nejc Sever <nejc.sever@outfit7.com>
2017-12-02 10:14:19 +0100

    Day 2: Corruption Checksum

--- src/day2/Day2.kt
+++ src/day2/Day2.kt
@@ -1,6 +1,5 @@
 package day2
 
-
 fun main(args: Array<String>) {
     val input = Object::class.java.getResource("/day2/input.txt").readText()
     val spreadsheet = input.split("\n").map { it.split("\t").map { it.toInt() } }
