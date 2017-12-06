f3f700438688a9104354c30682d1ef4332ea05e5
Nejc Sever <nejc.sever@outfit7.com>
2017-12-06 07:55:55 +0100

    Day 6: Memory Reallocation

--- src/day6/MemoryReallocator.kt
+++ src/day6/MemoryReallocator.kt
@@ -3,18 +3,18 @@ package day6
 object MemoryReallocator {
     fun reallocate(input: String): Pair<Int, Int> {
         var cells = input.split("\t").map { it.toInt() }.toMutableList()
-        val seenCombinations = mutableSetOf<List<Int>>()
+        val seenCombinations = mutableMapOf<List<Int>, Int>()
         var cyclesCount = 0
-        while (!seenCombinations.contains(cells)) {
+        while (!seenCombinations.containsKey(cells)) {
+            seenCombinations.put(cells.toList(), cyclesCount)
             cyclesCount++
-            seenCombinations.add(cells.toList())
             val (maxValue, maxIndex) = maxValueWithIndex(cells)
             cells[maxIndex] = 0
             for (i in 1..maxValue) {
                 cells[(maxIndex + i) % cells.size]++
             }
         }
-        return Pair(cyclesCount, cyclesCount - seenCombinations.indexOf(cells))
+        return Pair(cyclesCount, cyclesCount - seenCombinations[cells]!!)
     }
 
     private fun maxValueWithIndex(list: List<Int>): Pair<Int, Int> {
